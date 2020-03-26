feea1c68b9cebfe90c61d8ccbf5421d88fb301d2
RayToth <t.rajmund.98@gmail.com>
2020-03-26 10:05:56 +0100

    Get drives, folders, files

---
+++ SanityArchiver/SanityArchiver.DesktopUI/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using SanityArchiver.Application.Models;
+
+namespace SanityArchiver.DesktopUI.ViewModels
+{
+#pragma warning disable SA1600 // Elements should be documented
+    public class MainWindowViewModel
+    {
+        private static List<FileInfo> _files;
+        private static List<string> _directories;
+        private static List<string> _drivers;
+
+        public MainWindowViewModel()
+        {
+        }
+
+        public void GetDrives()
+        {
+            foreach (var drive in Directory.GetLogicalDrives())
+            {
+                _drivers.Add(drive);
+            }
+        }
+
+        public void GetDirectories(string fullPath)
+        {
+            var dirs = Directory.GetDirectories(fullPath);
+
+            if (dirs.Length > 0)
+            {
+                _directories.AddRange(dirs);
+            }
+        }
+
+        public string GetFileFolderName(string path)
+        {
+            if (string.IsNullOrEmpty(path))
+            {
+                return string.Empty;
+            }
+
+            var normalizedPath = path.Replace('/', '\\');
+
+            var lastIndex = normalizedPath.LastIndexOf('\\');
+
+            if (lastIndex <= 0)
+            {
+                return path;
+            }
+
+            return path.Substring(lastIndex + 1);
+        }
+    }
+#pragma warning restore SA1600 // Elements should be documented
+}
